45db4605d342cb56bbbb92d26f4464d5476700f8
stefanocoretta <s.coretta@ed.ac.uk>
2025-10-21 21:53:35 +0100

    add bootstrap

--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 window.electronAPI.onProjectOpened((projectPath) => {
   const pathElement = document.getElementById('project-path');
   pathElement.textContent = `Opened project: ${projectPath}`;
