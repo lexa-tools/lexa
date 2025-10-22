bbe70b0d93030064c7d84f2d9deb40b821a5622b
stefanocoretta <s.coretta@ed.ac.uk>
2025-10-22 11:46:07 +0100

    increase default window size

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ let mainWindow
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
