ff26baf0353edc0dbf5825a426783098e52d8448
Stefano Coretta <s.coretta@ed.ac.uk>
2025-10-25 18:46:24 +0100

    move db path to statusbar

--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,11 @@
 
+// Send lexadb path
 window.electronAPI.onLexadbOpened((lexadbPath) => {
   const pathElement = document.getElementById('lexadb-path');
-  pathElement.textContent = `Opened Lexa DB: ${lexadbPath}`;
+  pathElement.textContent = `${lexadbPath}`;
 });
 
+// Send lexadb validation
 window.electronAPI.onLexadbValidation((validation) => {
   const pathElement = document.getElementById('lexadb-validation');
   pathElement.textContent = `Valid: ${validation.valid}`;
