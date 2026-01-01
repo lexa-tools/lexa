3e4707cc89cb87100cce8ee052392b7286a0bbf7
Stefano Coretta <s.coretta@ed.ac.uk>
2026-01-01 14:42:27 +0000

    create renderer dir

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, 'renderer/index.html'));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
