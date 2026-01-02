c36673dc2730cbc26fac76b9c6af568aa8fbcfc7
Stefano Coretta <s.coretta@ed.ac.uk>
2026-01-02 18:14:53 +0000

    move openLexadb

--- src/main/db/openLexadb.js
+++ src/main/db/openLexadb.js
@@ -4,7 +4,7 @@ Licensed under the GNU GPL v3. See LICENSE file for details. */
 const { dialog, Menu } = require('electron');
 const validation = require('../validation');
 const read = require('../utils/readConfig');
-const lexicon = require('./lexiconStats');
+const lexicon = require('../lexicon/lexiconStats');
 const { getMainWindow } = require('../window');
 
 let currentLexadbPath = null;
--- src/main/menu.js
+++ src/main/menu.js
@@ -2,7 +2,7 @@
 Licensed under the GNU GPL v3. See LICENSE file for details. */
 
 const { Menu } = require('electron');
-const { openLexadb } = require('./lexicon/openLexadb');
+const { openLexadb } = require('./db/openLexadb');
 const { writeMerged } = require('./utils/writeMerged');
 
 function setupMenu() {
