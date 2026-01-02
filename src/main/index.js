95a176cdef7b25cf067e31151a195b474a18809e
Stefano Coretta <s.coretta@ed.ac.uk>
2026-01-02 10:15:41 +0000

    create src/main dir

--- src/main/index.js
+++ src/main/index.js
@@ -33,7 +33,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'ui/index.html'));
+  mainWindow.loadFile(path.join(__dirname, '../ui/index.html'));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
